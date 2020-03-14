2cf87a9779b1700adfc369bdc1cc4f405bafe324
raberin <rmvaberin@gmail.com>
2020-03-14 11:47:53 +0800

    Added footer icons

--- src/App.js
+++ src/App.js
@@ -1,27 +1,24 @@
 import React, { Component } from "react";
 import "./App.css";
-import {Route } from 'react-router-dom'
+import { Route } from "react-router-dom";
+
+// Components
 import Navigation from "./components/Navigation";
 import LogoContainer from "./components/LogoContainer";
 import LoginContainer from "./components/LoginContainer";
 import TrainingRoom from "./components/TrainingRoom";
 import LandingPage from "./components/LandingPage";
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Navigation />
-        <Route
-          exact
-          path="/"
-          render={props => <LandingPage {...props}/>}
-        />
-        
-        <Route
-          path="/demo"
-          render={props => <TrainingRoom {...props}/>}
-        />
+        <Route exact path="/" render={props => <LandingPage {...props} />} />
+
+        <Route path="/demo" render={props => <TrainingRoom {...props} />} />
+        <Footer />
       </div>
     );
   }
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import { NavLink } from "react-router-dom";
 const Footer = props => {
   return (
     <footer className="footer footerContainer">
-      <div className="footerNav">
+      <div className="footerTop">
         <NavLink to="/">HOME</NavLink>
         <NavLink to="/">SOFTWARE</NavLink>
         <NavLink to="/">DEMO</NavLink>
@@ -12,13 +12,30 @@ const Footer = props => {
         <NavLink to="/">INVEST</NavLink>
       </div>
       <div>
-        <div>
-          <p>
-            <strong>Archlife Industries© 2020</strong>
-          </p>
-          <a href="mailto:info@archlife.org">
-            <strong>info@Archlife.org</strong>
-          </a>
+        <div className="footerBottom">
+          <div className="footerCopyright">
+            <p>
+              <strong>Archlife Industries© 2020</strong>
+            </p>
+            <a href="mailto:info@archlife.org">
+              <strong>info@Archlife.org</strong>
+            </a>
+          </div>
+          <div className="footerLogo">
+            <img
+              src={require("../assets/Archlife-A-logo-new.png")}
+              width="300"
+              height="300"
+              alt="Large A logo of Archlife"
+              className="largeA"
+            />
+          </div>
+          <div className="footerSocial">
+            <div></div>
+            <div></div>
+            <div></div>
+            <div></div>
+          </div>
         </div>
       </div>
     </footer>
