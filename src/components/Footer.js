d05c3ce9aeea6384f2e35a4dbb4c06405dcc6554
raberin <rmvaberin@gmail.com>
2020-03-16 11:11:08 -0700

    Added color logo to nav and added links to footer social icons

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -47,32 +47,40 @@ const Footer = props => {
           </div>
           <div className="footerSocial">
             <div>
-              <img
-                src={require("../assets/Social_Icons/twitter.png")}
-                width="40"
-                height="40"
-              />
+              <a href="https://www.twitter.com/archlife3/">
+                <img
+                  src={require("../assets/Social_Icons/twitter.png")}
+                  width="40"
+                  height="40"
+                />
+              </a>
             </div>
             <div>
-              <img
-                src={require("../assets/Social_Icons/github.png")}
-                width="40"
-                height="40"
-              />
+              <a href="https://github.com/archlife-industries/">
+                <img
+                  src={require("../assets/Social_Icons/github.png")}
+                  width="40"
+                  height="40"
+                />
+              </a>
             </div>
             <div>
-              <img
-                src={require("../assets/Social_Icons/linkedin.png")}
-                width="40"
-                height="40"
-              />
+              <a href="https://www.linkedin.com/organization-guest/company/archlife-industries?challengeId=AQHLTOlJevuPfQAAAW9zG1_Vst-Avch9x0dPRtj_UMsRdL_YoIBgEXLCrmP8CG3-LmDsJfKq8efe_0PwMsdcwYGzqvw-qmZVbg&submissionId=45ffbe6a-24d6-e615-0657-105022afa23f">
+                <img
+                  src={require("../assets/Social_Icons/linkedin.png")}
+                  width="40"
+                  height="40"
+                />
+              </a>
             </div>
             <div>
-              <img
-                src={require("../assets/Social_Icons/youtube.png")}
-                width="40"
-                height="40"
-              />
+              <a href="https://www.youtube.com/channel/UCOHXyEqUG4ncPt1WPMzpm4Q/?guided_help_flow=5">
+                <img
+                  src={require("../assets/Social_Icons/youtube.png")}
+                  width="40"
+                  height="40"
+                />
+              </a>
             </div>
           </div>
         </div>
